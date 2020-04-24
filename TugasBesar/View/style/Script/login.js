dbd41de52479721ce6c9e647c6b24dbe4336b438
Michael Manurung <michaelzeros8@gmail.com>
2020-04-24 16:44:43 +0700

    External javascript for login

---
+++ TugasBesar/View/style/Script/login.js
@@ -0,0 +1,13 @@
+<script>
+  function checkLabel(labelID,input){
+    let label = document.getElementById(labelID);
+    input = input;
+    if(input.value == ""){
+      if(!label.classList.contains('label-active')){
+        label.classList.add('label-active');
+      }else{
+        label.classList.remove('label-active');
+      }
+    }
+  }
+</script>
