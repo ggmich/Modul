0d10c7f3484f11a122d3a71a81b7609dcd961d93
Michael Manurung <michaelzeros8@gmail.com>
2020-04-28 14:00:06 +0700

    salah database

--- TugasBesar/Controller/connection.php
+++ TugasBesar/Controller/connection.php
@@ -4,7 +4,7 @@ $connection = mysqli_connect('localhost','root','');
 if (!$connection){
     die("Database Connection Failed" . mysqli_error($connection));
 }
-$select_db = mysqli_select_db($connection, 'User');
+$select_db = mysqli_select_db($connection, 'togetherWeCan');
 if (!$select_db){
     die("Database Selection Failed" . mysqli_error($connection));
 }
