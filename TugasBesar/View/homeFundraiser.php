8d5016414303be1e5a48fe412c67b5a82edd6c0c
GGLuks <64063689+GGLuks@users.noreply.github.com>
2020-05-03 16:26:17 +0700

    gambar belum bisa tampil

---
+++ TugasBesar/View/homeFundraiser.php
@@ -0,0 +1,11 @@
+<?php 
+	
+?>
+<?php include "../View/style/headerUser.html" ?>
+
+	<table>
+		<tr>
+			<th><a href="../FundraiserNewProject.html"> <img src="../View/style/plus.png"></a>"</th>
+		</tr>
+	</table>
+	
