1def293a8c659f09db2ac420487cb9b27cbbc8e1
Michael Manurung <michaelzeros8@gmail.com>
2020-05-03 11:27:10 +0700

    Registration PHP work

--- TugasBesar/Controller/registration.php
+++ TugasBesar/Controller/registration.php
@@ -4,31 +4,33 @@
 */
 require ('../Model/connection.php');
 
+// re create from connection php, BUG
+$connection = mysqli_connect('localhost','root','');
+$select_db = mysqli_select_db($connection, 'togetherWeCan');
+
+
 // check html textBox empty or not
-/*
-if(isset($_POST['username']) and isset($_POST['password'])){
+if(isset($_POST['username']) and isset($_POST['email']) and isset($_POST['password'])){
 
   // initialize username & password from login form.html
   $username = $_POST['username'];
+  $email = $_POST['email'];
   $password = $_POST['password'];
 
-  // check credential in database
-  $checkQuery = "select * from User where username='$username' and password='$password'";
-  $result = mysqli_query($connection, $checkQuery) or die(mysqli_error($connection));
-  $count = mysqli_num_rows($result);
-
-  if($count == 1){
-    // Login Credentials verified
-    echo "<script type='text/javascript'>alert('Login Credentials verified')</script>";
-    header("Location: ../View/home.html");
-    exit();
+  // credential registration in database
+  $registerQuery = "INSERT INTO `User` (`userName`, `namaLengkap`, `email`, `password`, `noHp`) VALUES ('$username', '', '$email', '$password', '')
+";
 
 
-  }else{
-    // Login Credentials verified
-    echo "<script type='text/javascript'>alert('Invalid Login Credentials')</script>";
-
+  // $connection variable from connection.php
+  if(mysqli_query($connection, $registerQuery)){
+    echo "Records inserted successfully.";
+  } else{
+    echo "ERROR: Could not able to execute $registerQuery. " . mysqli_error($connection);
   }
+
+  // Close connection
+  mysqli_close($connection);
 }
-*/
+
 ?>
