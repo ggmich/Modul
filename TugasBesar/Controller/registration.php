32c08dd7a7a9cb5ea7dae641805cc5f9cd732443
Michael Manurung <michaelzeros8@gmail.com>
2020-05-05 14:21:00 +0700

    Update registration.php

--- TugasBesar/Controller/registration.php
+++ TugasBesar/Controller/registration.php
@@ -16,12 +16,29 @@ if(isset($_POST['username']) and isset($_POST['email']) and isset($_POST['passwo
   $registerQuery = "INSERT INTO `User` (`userName`, `namaLengkap`, `email`, `password`, `noHp`) VALUES ('$username', '', '$email', '$password', '')
 ";
 
-  // $connection variable from connection.php
-  if(mysqli_query($connection, $registerQuery)){
-    echo "Records inserted successfully.";
-  } else{
-    echo "ERROR: Could not able to execute $registerQuery. " . mysqli_error($connection);
+  // check credential in database
+  $checkQuery = "select * from User where username='$username' and password='$password'";
+  $result = mysqli_query($connection, $checkQuery) or die(mysqli_error($connection));
+  $count = mysqli_num_rows($result);
+
+  if($count >= 1){
+    // Login Credentials verified
+    echo "<script type='text/javascript'>alert('Invalid Login Credentials')</script>";
+    header("Location: ../View/index.php");
+    exit();
+
+  }else{
+
+    // $connection variable from connection.php
+    if(mysqli_query($connection, $registerQuery)){
+      echo "Records inserted successfully.";
+      header("Location: ../View/home.html");
+    } else{
+      echo "ERROR: Could not able to execute $registerQuery. " . mysqli_error($connection);
+    }
   }
+
+
 
   // Close connection
   mysqli_close($connection);
