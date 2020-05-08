406d10d7d9498c4850ca2422eb8c43a4ecaa57ae
Michael Manurung <michaelzeros8@gmail.com>
2020-05-08 16:41:48 +0700

    summary

--- TugasBesar/Controller/userController/fundraiserUser/fundraiserAddNewProject.php
+++ TugasBesar/Controller/userController/fundraiserUser/fundraiserAddNewProject.php
@@ -1,12 +1,36 @@
 <?php
+/*
+    Essential connection module for Controller Folder
+*/
+
+// query for connect to phpMyAdmin default Credentials
+$connection = mysqli_connect('localhost','root','');
+
+// if connection fail terminate
+if (!$connection){
+
+    die("Database Connection Failed" . mysqli_error($connection));
+}
+
+// connect to togetherWeCan Database in server
+$select_db = mysqli_select_db($connection, 'togetherWeCan');
+
+// if connection fail terminate
+if (!$select_db){
+    die("Database Selection Failed" . mysqli_error($connection));
+}
+
+?>
+
+<?php
 
 /*
     fundraiser add new project method
 */
-include ('../Model/connection.php');
+
 
 // check html textBox empty or not
-if(isset($_POST['username']) and isset($_POST['email']) and isset($_POST['password'])){
+if(isset($_POST['nama']) and isset($_POST['story']) and isset($_POST['image'])){
 
   // registering data to Campaign atribute database
   $name = $_POST['nama'];
@@ -17,7 +41,7 @@ if(isset($_POST['username']) and isset($_POST['email']) and isset($_POST['passwo
   $phone = $_POST['phone'];
   $address = $_POST['address'];
   $fundTarget = $_POST['fundTarget'];
-
+  $image = $_POST['image'];
   /*
 
   $idFundraiser = ? (Masukin ke tabel campaign )
@@ -27,14 +51,13 @@ if(isset($_POST['username']) and isset($_POST['email']) and isset($_POST['passwo
   */
 
   // query for database
-  $registerQuery = "INSERT INTO `campaign` (`idCampaign`, `namaCampaign`, `idFundraiser`, `tglMulai`, `tglSelesai`, `fundTarget`, `story`, `type`, `ktp`, `phone`, `address`)
-  VALUES ('', '$name', '', '$date', '', '$fundTarget', '$story', '$type', '$ktp', '$phone', '$address')";
+  $registerQuery = "INSERT INTO `campaign` (`idCampaign`, `namaCampaign`, `idFundraiser`, `tglMulai`, `tglSelesai`, `fundTarget`, `story`, `type`, `ktp`, `phone`, `image`, `campaignLink`, `address`)
+  VALUES ('', '$name', '', '$date', '', '$fundTarget', '$story', '$type', '$ktp', '$phone', '$image', NULL, '$address')";
 
 
   // $connection variable from connection.php
   if(mysqli_query($connection, $registerQuery)){
     echo "Records inserted successfully.";
-    header("Location: ../View/home.html");
   } else{
     echo "ERROR: Could not able to execute $registerQuery. " . mysqli_error($connection);
   }
