a9aacea327d75824500969dcb182d1cf4c1c0cc9
Michael Manurung <michaelzeros8@gmail.com>
2020-05-09 13:56:43 +0700

    Organizing MVC Model

--- TugasBesar/Controller/userController/userDonatur/projectFeeds.php
+++ TugasBesar/Controller/userController/userDonatur/projectFeeds.php
@@ -4,6 +4,33 @@
     Update HTML function from database
     Criteria: - Listed Project              
 */
-
-
+    include "../Model/connection.php";
  ?>
+
+<?php 
+
+			$imageSelectQuery = " SELECT image, campaignLink, namaCampaign from campaign";
+			$result = $connection-> query($imageSelectQuery);
+
+			if($result-> num_rows > 0){
+				while($row = $result-> fetch_assoc()){
+					echo "<tr><td>";
+		?>
+
+		<?php
+					echo '<img src="data:image:base64,'.base64_encode($row['image']).'">';
+		?>
+
+		<?php			
+					echo "</td><td>";
+					echo "<a href='".$row["campaignLink"]."'>";
+					echo $row["namaCampaign"]."</a>";
+					echo "</td></tr>";
+				}
+			}
+			else{
+				echo "gagal fetching";
+			}
+
+			$connection-> close();
+		?>
--- TugasBesar/View/home.php
+++ TugasBesar/View/home.php
@@ -1,42 +1,13 @@
 <?php
 	include "../View/headerUser.html";
-  include "../Model/connection.php";
 ?>
 
 	<hr>
   <br>
 
 	<table>
-		<?php 
-
-			$imageSelectQuery = " SELECT image, campaignLink, namaCampaign from campaign";
-			$result = $connection-> query($imageSelectQuery);
-
-			if($result-> num_rows > 0){
-				while($row = $result-> fetch_assoc()){
-					echo "<tr><td>";
-		?>
-
-		<?php
-					echo '<img src="data:image:base64,'.base64_encode($row['image']).'">';
-		?>
-
-		<?php			
-					echo "</td><td>";
-					echo "<a href='".$row["campaignLink"]."'>";
-					echo $row["namaCampaign"]."</a>";
-					echo "</td></tr>";
-				}
-			}
-			else{
-				echo "gagal fetching";
-			}
-
-			$connection-> close();
-		?>
+		<?php include "../Controller/userController/userDonatur/projectFeeds.php"; ?>
 	</table>
-
-
 
 <?php
 
