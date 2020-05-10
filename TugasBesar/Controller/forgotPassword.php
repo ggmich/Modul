61fbb631d3a411bf6b0760e71421c594bfe3fe95
GGLuks <64063689+GGLuks@users.noreply.github.com>
2020-05-10 13:15:02 +0700

    forgotpassword controller

--- TugasBesar/Controller/forgotPassword.php
+++ TugasBesar/Controller/forgotPassword.php
@@ -4,31 +4,30 @@
 */
 require ('../Model/connection.php');
 
-/*
+
 // check html textBox empty or not
-if(isset($_POST['username']) and isset($_POST['password'])){
 
-  // initialize username & password from login form.html
-  $username = $_POST['username'];
-  $password = $_POST['password'];
+require('PHPMailer/PHPMailerAutoload.php');
+if (isset($_POST) & !empty($_POST)) {
+    $username = mysqli_real_escape_string($connection, $_POST['username']);
+    $sql = "SELECT * FROM `login` WHERE username = '$username'";
+    $res = mysqli_query($connection, $sql);
+    $count = mysqli_num_rows($res);
+    if ($count == 1) {
+        $r = mysqli_fetch_assoc($res);
+        $password = $r['password'];
+        $to = $r['email'];
+        $subject = "Your Recovered Password";
 
-  // check credential in database
-  $checkQuery = "select * from User where username='$username' and password='$password'";
-  $result = mysqli_query($connection, $checkQuery) or die(mysqli_error($connection));
-  $count = mysqli_num_rows($result);
-
-  if($count == 1){
-    // Login Credentials verified
-    echo "<script type='text/javascript'>alert('Login Credentials verified')</script>";
-    header("Location: ../View/home.html");
-    exit();
-
-
-  }else{
-    // Login Credentials verified
-    echo "<script type='text/javascript'>alert('Invalid Login Credentials')</script>";
-
-  }
+        $message = "Please use this password to login " . $password;
+        $headers = "From : TogetherWeCan.com";
+        if (mail($to, $subject, $message, $headers)) {
+            echo "Your Password has been sent to your email id";
+        } else {
+            echo "Failed to Recover your password, try again";
+        }
+    } else {
+        echo "User name does not exist in database";
+    }
 }
-*/
 ?>
