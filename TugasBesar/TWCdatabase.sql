634051d0bf1b6e427ca214c66859e3b2693e8b48
Michael Manurung <michaelzeros8@gmail.com>
2020-05-10 14:33:20 +0700

    Update TWCdatabase.sql

--- TugasBesar/TWCdatabase.sql
+++ TugasBesar/TWCdatabase.sql
@@ -69,7 +69,7 @@ CREATE TABLE IF NOT EXISTS User (
 
 
 ALTER TABLE User
-  ADD PRIMARY KEY (userName);
+  ADD PRIMARY KEY (idUser);
 
   CREATE  UNIQUE INDEX Cmpgn ON campaign(idCampaign) USING BTREE;
  CREATE  UNIQUE INDEX fnraiser ON fundraiser(idFundraiser) USING BTREE;
