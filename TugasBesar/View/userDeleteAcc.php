7eef75ff622c6a42503a80d2545dc39a7cf435b8
GGLuks <64063689+GGLuks@users.noreply.github.com>
2020-05-13 19:24:50 +0700

    inactive

---
+++ TugasBesar/View/userDeleteAcc.php
@@ -0,0 +1,19 @@
+<?php
+	include "../View/headerUser.html";
+?>
+
+
+  <br>
+
+
+	<table>
+        <?php include "../Controller/userChangeStatus.php"; ?>
+        <button> Inactive Account</button>
+	</table>
+	
+
+<?php
+
+	include "../View/footer.html";
+
+?>
