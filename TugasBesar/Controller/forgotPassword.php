d90b5bf6dbfc886311320352fe7b7e00e6d6aec6
Michael Manurung <michaelzeros8@gmail.com>
2020-05-13 22:04:49 +0700

    Update forgotPassword.php

--- TugasBesar/Controller/forgotPassword.php
+++ TugasBesar/Controller/forgotPassword.php
@@ -4,11 +4,12 @@
 */
 require ('../Model/connection.php');
 
-
 // check html textBox empty or not
 
 require ('../Controller/userController/userDonatur/PHPMailerAutoLoad.php');
-if (isset($_POST['username']) & !empty($_POST['username'])) {
+if (isset($_POST['username'])) {
+
+
     $username = $_POST['username'];
     $sql = "select * from User where username='$username'";
     $res = mysqli_query($connection, $sql);
@@ -31,4 +32,7 @@ if (isset($_POST['username']) & !empty($_POST['username'])) {
         echo "User name does not exist in database";
     }
 }
+
+echo "<script type='text/javascript'>alert('Sudah kami kirim link reset ke email anda')</script>";
+header("Refresh:2;url=http://192.168.64.2/Modul/TugasBesar/View/");
 ?>
