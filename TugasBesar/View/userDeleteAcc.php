2a4416b4bcf63da04a6307198149db94c3171d91
GGLuks <64063689+GGLuks@users.noreply.github.com>
2020-05-13 19:53:34 +0700

    inactivate acc

--- TugasBesar/View/userDeleteAcc.php
+++ TugasBesar/View/userDeleteAcc.php
@@ -1,19 +1,34 @@
 <?php
-	include "../View/headerUser.html";
+include "../View/headerUser.html";
 ?>
 
 
-  <br>
+<br>
 
 
-	<table>
-        <?php include "../Controller/userChangeStatus.php"; ?>
-        <button> Inactive Account</button>
-	</table>
-	
+
+<?php include "../Controller/userChangeStatus.php"; ?>
+
+
+
+<div class="divIn">
+    <form method="post" action="../Controller/userChangeStatus.php">
+
+      
+    <label id="label-1" style="font-size: 20px;">Apakah anda yakin ingin mendeactivate Account?</label>
+            <select id="tipe" name="type"><br>
+            <option value="0">Ya</option>
+            <option value="1">Tidak</option>
+
+        <input class="button" type="submit" value="donasi"><br>
+
+
+    </form>
+</div>
+
 
 <?php
 
-	include "../View/footer.html";
+include "../View/footer.html";
 
 ?>
